bf721420f0e16b10127c64773f9c7b2e0709eb49
Gisele Macieira <giselefchaves@gmail.com>
2023-03-31 16:31:33 -0300

    versão com a seleção de prato, bebida e sobremesa finalizado

--- script.js
+++ script.js
@@ -11,11 +11,48 @@ function checkPrato(seletor1){
 function selecionarPrato(seletor){
     checkPrato(seletor);
     const pratoSelecionadoAnteriormente = document.querySelector('.prato' + '.selecionado');
-    console.log(pratoSelecionadoAnteriormente)
     if (pratoSelecionadoAnteriormente !== null){
         pratoSelecionadoAnteriormente.classList.remove('selecionado');
     }
     //encontrar o novo prato selecionado
-    const pratoSelecionado = document.querySelector(seletor) //seletor sendo .prato1 ou .prato2 ...
+    const pratoSelecionado = document.querySelector(seletor); //seletor sendo .prato1 ou .prato2 ...
     pratoSelecionado.classList.add('selecionado');
+}
+function checkBebida(seletor1){
+    const checkAnterior = document.querySelector('.bebida' + '.checkon');
+    if (checkAnterior !== null){
+        checkAnterior.classList.add('escondido');
+        checkAnterior.classList.remove('checkon');
+    }
+    const check = document.querySelector('.check' + seletor1);
+    check.classList.remove('escondido');
+    check.classList.add('checkon');
+}
+function selecionarBebida(seletor){
+    checkBebida(seletor);
+    const bebidaSelecionadoAnteriormente = document.querySelector('.bebida' + '.selecionado');
+    if (bebidaSelecionadoAnteriormente !== null){
+        bebidaSelecionadoAnteriormente.classList.remove('selecionado');
+    }
+    const bebidaSelecionado = document.querySelector(seletor);
+    bebidaSelecionado.classList.add('selecionado');
+}
+function checkSobremesa(seletor1){
+    const checkAnterior = document.querySelector('.sobremesa' + '.checkon');
+    if (checkAnterior !== null){
+        checkAnterior.classList.add('escondido');
+        checkAnterior.classList.remove('checkon');
+    }
+    const check = document.querySelector('.check' + seletor1);
+    check.classList.remove('escondido');
+    check.classList.add('checkon');
+}
+function selecionarSobremesa(seletor){
+    checkSobremesa(seletor);
+    const sobremesaSelecionadoAnteriormente = document.querySelector('.sobremesa' + '.selecionado');
+    if (sobremesaSelecionadoAnteriormente !== null){
+        sobremesaSelecionadoAnteriormente.classList.remove('selecionado');
+    }
+    const sobremesaSelecionado = document.querySelector(seletor);
+    sobremesaSelecionado.classList.add('selecionado');
 }
